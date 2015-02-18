4cf15e98cd50944705cb34e4036a789a2e6f35ad
Shugar <shugar348@gmail.com>
2015-02-18 17:36:52 +0300

    lol2

---
+++ app/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.close').click(function(){
+		$('.news').css("display", "none");
+	});
+});
+
+$(document).ready(function(){
+	$('.close-2').click(function(){
+		$('.spec').css("display", "none");
+	});
+});
+
